64a871a79d3fd23496e234da9e5a0b7b9fa88560
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-03 22:53:06 +0530

    [add] Shubham added AddressBookSystemLINQ program

--- AddressBookSystemLINQ/AddressBookTable.cs
+++ AddressBookSystemLINQ/AddressBookTable.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 
 namespace AddressBookSystemLINQ
 {
-    internal class AddressBookTable
+    class AddressBookTable
     {
+        // UC 1 Create a new DataTable
+        DataTable table = new DataTable("AddressBookSystem"); 
     }
 }
--- AddressBookSystemLINQ/Program.cs
+++ AddressBookSystemLINQ/Program.cs
@@ -6,7 +6,7 @@ namespace AddressBookSystemLINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Address Book System Using LINQ ");
         }
     }
 }
