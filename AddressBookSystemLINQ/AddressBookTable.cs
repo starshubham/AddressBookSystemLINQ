40b3719c7197c1d26e00b61f660ff2d7f6b8f8ac
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-03 23:13:23 +0530

    [add] Shubham added UC2 of AddressBookSystemLINQ program

--- AddressBookSystemLINQ/AddressBookTable.cs
+++ AddressBookSystemLINQ/AddressBookTable.cs
@@ -5,6 +5,21 @@ namespace AddressBookSystemLINQ
     class AddressBookTable
     {
         // UC 1 Create a new DataTable
-        DataTable table = new DataTable("AddressBookSystem"); 
+        DataTable table = new DataTable("AddressBookSystem");
+
+        /* UC2:- Ability to create a Address Book Table with first and last names, address, city, 
+                 state, zip, phone number and email as its attributes
+        */
+        public  AddressBookTable()
+        {
+            table.Columns.Add("FirstName", typeof(string));   //add columns
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+        }
     }
 }
